c9e5eaba250800947fd7177f77316043f87ca3db
Mobina Aghaeimaleki <120053168+mobina22am@users.noreply.github.com>
2025-01-29 11:44:39 +0000

    completing the models.py file

--- rhythmic/musicapp/models.py
+++ rhythmic/musicapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 
-class User(models.Model):
+class User(AbstractUser):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     birthday = models.DateField()
@@ -14,13 +14,23 @@ class User(models.Model):
 
 
 
-
 class StoredSongs(models.Model):
     name = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     details = models.TextField()
     notes = models.TextField()
+    instrument = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
         return f"{self.name}"
+    
+
+
+
+
+# user = User.objects.get(id=1)  # Get the user
+# song = StoredSongs.objects.get(user=user, name="Song Title")
+# print(song.details)
